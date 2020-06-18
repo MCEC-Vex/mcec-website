ec3995e69094b7839574d858ec9dd29ef69971bf
TheTechdoodle <admin+github@darkender.com>
2020-06-18 15:21:38 -0700

    further improve accessibility on unlabeled links

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,5 +1,5 @@
 <template>
-    <a href="#" class="text-copy-primary hover:text-gray-600" @click.prevent="toggleTheme">
+    <a href="#" class="text-copy-primary hover:text-gray-600" @click.prevent="toggleTheme" aria-label="theme switcher">
         <svg v-if="theme === 'theme-light'" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
              class="feather feather-moon">
