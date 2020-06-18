4fb9db06de5ed953b9a7156cecdb1a7aa4fa617b
TheTechdoodle <admin+github@darkender.com>
2020-06-18 14:43:42 -0700

    change meta tags

--- src/main.js
+++ src/main.js
@@ -20,14 +20,12 @@ export default function(Vue, {router, head, isClient})
     
     head.meta.push({
         name: 'keywords',
-        content: 'Gridsome,Vue,Tailwind,Tailwind CSS,JavaScript,HTML,CSS,Vue.js,VueJS'
+        content: 'Moorpark,College,Vex,Robotics,Engineering,Club'
     });
-    
     head.meta.push({
         name: 'description',
-        content: 'Gridsome Portfolio Starter'
+        content: 'Moorpark College Engineering Club Website'
     });
-    
     head.meta.push({
         name: 'author',
         content: 'Moorpark College Engineering Club'
