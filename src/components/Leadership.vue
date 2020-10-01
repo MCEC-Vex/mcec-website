5aa4b1ae251bbabb17ecb14bc604aaf6cc4cc1ac
Techdoodle <chris@techchrism.me>
2020-10-01 16:15:26 -0700

    add vice president, support for multiline bios, and support for no picture

--- src/components/Leadership.vue
+++ src/components/Leadership.vue
@@ -3,16 +3,18 @@
         <h2 class="font-bold mb-6" id="leadership">Club Leadership:</h2>
         <ul class="text-lg sm:text-xl space-y-6">
             <li v-for="leader of leadership" :key="leader.name" class="flex">
-                <g-image
+                <div class="leadership-image inline-block mr-4 mt-2">
+                    <g-image
                         v-if="leader.image"
                         :alt="leader.name"
-                        class="leadership-image inline-block rounded-full text-white shadow-solid mr-4 mt-2"
+                        class="rounded-full shadow-solid"
                         :src="require(`!!assets-loader!@leadership/${leader.image}`)"
-                />
+                    />
+                </div>
                 <div class="flex-1">
                     <div>{{leader.name}} - {{leader.position}}</div>
-                    <div class="text-lg text-gray-600">
-                        {{leader.bio}}
+                    <div class="text-lg text-gray-600" v-for="line of leader.bio.split('\n')" :key="line">
+                        {{line}}
                     </div>
                 </div>
             </li>
@@ -30,6 +32,16 @@
                     position: 'President',
                     bio: 'Hey guys! I am a third year Mechanical Engineering major. I\'m a local here in Moorpark and found this club as a great opportunity to engage hands-on and explore VEX Robotics, my newfound passion. I hope to inspire you as well. I look forward to seeing you this year!',
                     image: 'khoi_le.png'
+                },
+                {
+                    name: 'Wesley Karkiewicz',
+                    position: 'Vice President',
+                    bio: 'Pursuing a degree in Electrical engineering \n' +
+                        'Second year attending moorpark community college\n' +
+                        'Competed in a highschool robotics program called FIRST\n' +
+                        'Participated in our clubs first year back into vex by building the second robot and driving it at competitions.\n' +
+                        'Working a internship at Skurka aerospace \n' +
+                        'Currently in the Marine Corps Officer candidate program with plans on becoming commissioned in the service as a second lieutenant after graduating college and attending flight school.'
                 },
                 {
                     name: 'Luke Barrella',
